9f6a5842015895f85f9edef4d22654cc25afe65c
Rohan Malhotra <rohan.root@gmail.com>
2018-09-05 09:15:51 +0530

    changed db to pg

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[show update destroy]
+  before_action :set_post, only: %i[update destroy]
 
   # GET /posts
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_09_04_091430) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "message"
     t.integer "user_id"
